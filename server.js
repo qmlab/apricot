83bfec573b8ba739a89168b4f2b5083d06059cf1
qmstudio <mainchin@msn.com>
2014-12-16 18:07:42 -0800

    small modifications

--- server.js
+++ server.js
@@ -9,42 +9,42 @@ var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
 
 app.param('collectionName', function(req, res, next, collectionName){
   req.collection = db.collection(collectionName)
-  return next()
+  next()
 })
 
 app.get('/', function(req, res, next) {
-  res.send('please select a collection, e.g., /collections/messages')
+  res.send('please select a collection, e.g., /colls/messages')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
+app.get('/colls/:collectionName', function(req, res, next) {
   req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
       res.send(results)
     })
   })
 
-  app.post('/collections/:collectionName', function(req, res, next) {
+  app.post('/colls/:collectionName', function(req, res, next) {
     req.collection.insert(req.body, {}, function(e, results){
       if (e) return next(e)
         res.send(results)
       })
     })
 
-    app.get('/collections/:collectionName/:id', function(req, res, next) {
+    app.get('/colls/:collectionName/:id', function(req, res, next) {
       req.collection.findById(req.params.id, function(e, result){
         if (e) return next(e)
           res.send(result)
         })
       })
 
-      app.put('/collections/:collectionName/:id', function(req, res, next) {
+      app.put('/colls/:collectionName/:id', function(req, res, next) {
         req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
           if (e) return next(e)
             res.send((result===1)?{msg:'success'}:{msg:'error'})
           })
         })
 
-        app.del('/collections/:collectionName/:id', function(req, res, next) {
+        app.del('/colls/:collectionName/:id', function(req, res, next) {
           req.collection.removeById(req.params.id, function(e, result){
             if (e) return next(e)
               res.send((result===1)?{msg:'success'}:{msg:'error'})
